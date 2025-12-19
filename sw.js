9b9ec024a3109626d6bfb04bd6db21ec3eb41146
jordangeiman-design <jordan.geiman@gmail.com>
2025-12-18 16:18:06 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,20 @@
-const CACHE_NAME = 'metcon-v2'; // Bumped version to clear old black screen cache
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './index.tsx',
-  './App.tsx',
-  './types.ts',
-  './constants.ts'
-];
+const CACHE_NAME = 'metcon-v3';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys.map(key => caches.delete(key))
       );
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Network first strategy for scripts to ensure updates work
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  // Pure network strategy for now to ensure we see the latest fixes
+  event.respondWith(fetch(event.request));
 });
